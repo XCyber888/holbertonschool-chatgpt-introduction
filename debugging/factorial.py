510236646bae2c0d7981f0209eba115167f0e99e
XCyber888 <xalidyusifov744@gmail.com>
2026-02-08 17:07:20 +0000

    Add factorial debugging task

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-
 import sys
 
-
-
 def factorial(n):
-
     result = 1
-
     while n > 1:
-
         result *= n
-
         n -= 1
-
     return result
 
-
-
-f = factorial(int(sys.argv[1]))
-
-print(f)
-# Factorial calculation script
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        f = factorial(int(sys.argv[1]))
+        print(f)
